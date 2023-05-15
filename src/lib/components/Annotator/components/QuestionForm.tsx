b8f9bdbea0f1b0e38519a694d2d6b58d07c1b2b9
Kasper Welbers <kasperwelbers@gmail.com>
2023-05-15 14:38:01 +0200

    cleanup

--- src/lib/components/Annotator/components/QuestionForm.tsx
+++ src/lib/components/Annotator/components/QuestionForm.tsx
@@ -129,14 +129,12 @@ const QuestionForm = ({
   const blockAnswer = useRef(false); // to prevent answering double (e.g. with swipe events)
   const [answers, setAnswers] = useState<Answer[]>(null);
   const [questionText, setQuestionText] = useState(<div />);
-  const questionTimer = useRef<number>(new Date().getTime());
 
   if (useWatchChange([unit, questions])) {
     if (questions) setAnswers(getAnswersFromAnnotations(unit, questions));
   }
 
   if (useWatchChange([unit, questions, answers, questionIndex])) {
-    questionTimer.current = new Date().getTime();
     if (!questions?.[questionIndex] || !unit) {
       setQuestionIndex(0);
     } else {
@@ -177,8 +175,7 @@ const QuestionForm = ({
         setQuestionIndex,
         setConditionReport,
         (nextUnit: boolean) => startTransition(transition, nextUnit),
-        blockAnswer,
-        questionTimer.current
+        blockAnswer
       );
     },
     [
@@ -191,7 +188,6 @@ const QuestionForm = ({
       unit,
       setSwipe,
       startTransition,
-      questionTimer,
     ]
   );
 
@@ -306,8 +302,7 @@ const processAnswer = async (
   setQuestionIndex: SetState<number>,
   setConditionReport: SetState<ConditionReport>,
   transition: (nextUnit: boolean) => void,
-  blockAnswer: any,
-  questionTimer: number
+  blockAnswer: any
 ): Promise<void> => {
   if (blockAnswer.current) return null;
   blockAnswer.current = true;
@@ -320,7 +315,6 @@ const processAnswer = async (
     );
 
     unit.unit.annotations = addAnnotationsFromAnswer(answers[questionIndex], unit.unit.annotations);
-    console.log(unit.unit.annotations);
     const irrelevantQuestions = processIrrelevantBranching(unit, questions, answers, questionIndex);
 
     // next (non-irrelevant) question in unit (null if no remaining)
