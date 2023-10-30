8b68d3de52a3e73cc77cd955d82f233a850aa178
Kasper Welbers <kasperwelbers@gmail.com>
2023-10-30 09:10:30 +0100

    highlighting with markdown code tags

--- src/lib/components/Common/Markdown.tsx
+++ src/lib/components/Common/Markdown.tsx
@@ -10,32 +10,21 @@ const Markdown = ({ children, style = {} }: MarkdownProps) => {
   return (
     <div style={{ ...style }}>
       <ReactMarkdown
-        // components={{
-        //   // Map `h1` (`# heading`) to use `h2`s.
-        //   h1: "h2",
-        //   // Rewrite `em`s (`*like so*`) to `i` with a red foreground color.
-        //   em(props) {
-        //     const { node, ...rest } = props;
-        //     return <i style={{ color: "red" }} {...rest} />;
-        //   },
-        // }}
-        // components={{
-        //   code(props) {
-        //     const { children, className, node, ...rest } = props;
-        //     const match = /language-(\w+)/.exec(className || "");
-        //     return match ? (
-        //       <span
-        //         {...rest}
-        //         children={String(children).replace(/\n$/, "")}
-        //         style={{ backgroundColor: "lightgrey", padding: "2px 5px" }}
-        //       />
-        //     ) : (
-        //       <code {...rest} className={className}>
-        //         {children}
-        //       </code>
-        //     );
-        //   },
-        // }}
+        components={{
+          code(props) {
+            const { node, ...rest } = props;
+            return (
+              <span
+                style={{
+                  background: "var(--primary-light)",
+                  padding: "0.3rem",
+                  borderRadius: "5px",
+                }}
+                {...rest}
+              />
+            );
+          },
+        }}
         linkTarget={"_blank"}
       >
         {children}
