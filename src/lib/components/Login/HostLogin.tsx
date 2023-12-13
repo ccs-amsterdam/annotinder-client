fed5b5cfb7c778836d6f2db95c70645629d8fee0
Kasper Welbers <kasperwelbers@gmail.com>
2023-12-13 15:24:10 +0100

    change fixed host

--- src/lib/components/Login/HostLogin.tsx
+++ src/lib/components/Login/HostLogin.tsx
@@ -22,7 +22,7 @@ export const HostLogin = ({
   hostInfoQuery,
 }: HostLoginProps) => {
   let fixedHost = "";
-  fixedHost = "https://vertrouwen.up.railway.app";
+  fixedHost = "https://annotinder.up.railway.app";
   const [hostInput, setHostInput] = useState(fixedHost || host);
   const [emailInput, setEmailInput] = useState(email);
   const [invalidEmail, setInvalidEmail] = useState(false);
